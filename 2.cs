1afbfedc07cc6991cdff60134309e6c6402f354e
sibgutiStudentRI12GrM <90037572+sibgutiStudentRI12GrM@users.noreply.github.com>
2021-09-03 17:40:52 +0700

    Create 2.cs

---
+++ 2.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            while (true) {
+                Console.Write(">>> ");
+                string input = Console.ReadLine();
+                int value;
+                if(int.TryParse(input, out value)){
+                    Console.WriteLine("Is number!");
+                } else {
+                    Console.WriteLine("Not a number!");
+                }
+            }
+        }
+    }
+}
+
+
+/*
+ Result:
+    >>> text
+    Not a number!
+    >>> 1 
+    Is number!
+    >>> 0
+    Is number!
+    >>> A big text
+    Not a number!
+*/
